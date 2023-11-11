be36009384cd9762d7712156a88668cce8dd0baf
enzosorcini <enzosorcini.w@gmail.com>
2023-11-11 13:25:15 +0100

    my tests

---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,25 @@
+describe("Newsletter subscribe form", () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+    })
+
+    it("allows users to subscribe to the email list", () => {
+        cy.getByData('email-input').type("some@mail.com")
+        cy.getByData('submit-button').click()
+        cy.getByData('success-message').should('exist').contains("some@mail.com")
+    })
+
+    it("does NOT allow an invalid email address", () => {
+        cy.getByData('email-input').type("lalala")
+        cy.getByData('submit-button').click()
+        cy.getByData('success-message').should('not.exist')
+    })
+
+    it.only("does not allow subscriptions from already subscribed emails", () => {
+        cy.getByData('email-input').type("john@example.com")
+        cy.getByData('submit-button').click()
+        cy.getByData('server-error-message')
+            .should('exist')
+            .contains("john@example.com already exists")
+    })
+})
